95da6821f9077e372fcae80995af52e2bd9f5f57
Darlin <darlingtonablorh7@gmail.com>
2022-08-09 14:37:58 +0200

    Create and display data boxes

--- graphic_PCF.py
+++ graphic_PCF.py
@@ -2,6 +2,7 @@ import mysql.connector as sqlc
 import pandas as pd
 from tkinter import *
 
+# Establish connection to the database
 cnx = sqlc.connect(
     user="root",
     password="TheDoctor3005",
@@ -9,14 +10,46 @@ cnx = sqlc.connect(
     database="perez"
 )
 
-
+# Create a cursor
 mycursor = cnx.cursor()
 
 
+# Config the GUI
 root = Tk()
 root.title('CLINIC DATA FINDER')
-root.geometry("800x800")
+root.geometry("400x400")
 
+# Create the head message
+welcome = Label(root, width=50, text="CLINIC DATA FINDER")
+welcome.grid(row=0, column=0, columnspan=2)
+
+# Create the data boxes
+name = Entry(root, width=30)
+name.grid(row=1, column=1, padx=20, pady=5)
+surname = Entry(root, width=30)
+surname.grid(row=2, column=1, padx=20, pady=5)
+id = Entry(root, width=30)
+id.grid(row=3, column=1, padx=20, pady=5)
+bd = Entry(root, width=30)
+bd.grid(row=4, column=1, padx=20, pady=5)
+age = Entry(root, width=30)
+age.grid(row=5, column=1, padx=20, pady=5)
+sex = Entry(root, width=30)
+sex.grid(row=6, column=1, padx=20, pady=5)
+
+# Create the data box labels
+name_label = Label(root, text="First name:")
+name_label.grid(row=1, column=0)
+surname_label = Label(root, text="Last name:")
+surname_label.grid(row=2, column=0)
+id_label = Label(root, text="ID number:")
+id_label.grid(row=3, column=0)
+bd_label = Label(root, text="Date of birth\n(YYYY-MM-DD):")
+bd_label.grid(row=4, column=0)
+age_label = Label(root, text="Current age:")
+age_label.grid(row=5, column=0)
+sex_label = Label(root, text="Gender:")
+sex_label.grid(row=6, column=0)
 
 
 
