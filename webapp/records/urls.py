2adc4ceec6d6e348f4dbebbe0f9561d0faa8d4e2
Darlin <darlingtonablorh7@gmail.com>
2022-09-01 18:15:01 +0200

    Improve Add-patient template

--- webapp/records/urls.py
+++ webapp/records/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.main_page),
-    path('patient/', views.addp_page),
+    path('', views.main_page, name="home"),
+    path('patient/', views.addp_page, name="new_p"),
 ]
