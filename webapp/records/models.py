2e644c70da9f0166b67dc4eac25641dfa4492cf7
Darlin <darlingtonablorh7@gmail.com>
2022-09-03 17:24:31 +0200

    Add Patient model class

--- webapp/records/models.py
+++ webapp/records/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Patient(models.Model):
+    patient_ID = models.IntegerField(null=False, primary_key=True)
+    name = models.CharField(null=False, max_length=20)
+    surname = models.CharField(null=False, max_length=20)
+    bd = models.DateField()
+    gender = models.CharField(max_length=6)
+
+
+    def __str__(self):
+        return (self.name + " " + self.surname) 
+
+
+
