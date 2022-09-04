b2e77fb37e7afe4e2b0dcdc1e0ac14566dc5738b
Darlin <darlingtonablorh7@gmail.com>
2022-09-04 19:04:16 +0200

    Improve Add-patient crispy form

--- webapp/records/forms.py
+++ webapp/records/forms.py
@@ -3,16 +3,22 @@ from .models import Patient, CBC
 from datetime import date, datetime
 from django.urls import reverse_lazy
 from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Submit
+from crispy_forms.layout import Submit, Layout, Field
 
 
 class PatientForm(ModelForm):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.helper = FormHelper(self)
+        helper = self.helper = FormHelper(self)
         self.helper.form_action = reverse_lazy('home')
         self.helper.add_input(Submit('submit', 'ADD NEW PATIENT'))
+
+        # Moving field labels into placeholders
+        layout = helper.layout = Layout()
+        for field_name, field in self.fields.items():
+            layout.append(Field(field_name, placeholder=field.label))
+        helper.form_show_labels = False
 
     class Meta:
         model = Patient
